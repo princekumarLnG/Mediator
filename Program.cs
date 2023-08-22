4b75375e883b0cc8e58d7d706b3bca007bc4fec0
Prince Bhagat <130346200+princekumarLnG@users.noreply.github.com>
2023-08-22 17:51:15 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -23,11 +23,14 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseSwagger();
+app.UseSwaggerUI();
 if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
 
